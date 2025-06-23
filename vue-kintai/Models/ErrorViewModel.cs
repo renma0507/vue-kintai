9ed7e7ced11ba7cb3b0c48efa37d10601c909266
renma <renma0507@ezweb.ne.jp>
2025-06-23 13:08:02 +0900

    はじめました

---
+++ vue-kintai/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace vue_kintai.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
