6d4bdad7e6d709d160945820503bfd32d0bb1d0c
renma <renma0507@ezweb.ne.jp>
2025-06-24 14:38:54 +0900

    山田コミット4

--- vue-kintai-js/src/views/KanriView.vue
+++ vue-kintai-js/src/views/KanriView.vue
@@ -49,7 +49,7 @@ function applyEdits() {
     <div class="main">
       <!-- 日付 -->
       <div class="date-input" >
-        <label>日付:</label>
+        <!-- <label>日付:</label> -->
         <input type="date" v-model="selectedDate" class="date"/>
       </div>
 
@@ -76,10 +76,10 @@ function applyEdits() {
           </thead>
           <tbody>
             <tr>
-              <td><input type="time" v-model="editedTime.startTime" /></td>
-              <td><input type="time" v-model="editedTime.endTime" /></td>
-              <td><input type="time" v-model="editedTime.breakStart" /></td>
-              <td><input type="time" v-model="editedTime.breakEnd" /></td>
+              <td><input type="time" class="bigtime"v-model="editedTime.startTime" /></td>
+              <td><input type="time"  class="bigtime"v-model="editedTime.endTime" /></td>
+              <td><input type="time"  class="bigtime"v-model="editedTime.breakStart" /></td>
+              <td><input type="time" class="bigtime" v-model="editedTime.breakEnd" /></td>
             </tr>
           </tbody>
         </table>
@@ -94,6 +94,11 @@ function applyEdits() {
 </template>
 
 <style scoped>
+.bigtime{
+  width: 70%;
+  height: 100px;
+  font-size: 45px;
+}
 .container {
   display: flex;
   height: 100vh;
@@ -111,15 +116,18 @@ function applyEdits() {
 /* 日付入力 */
 .date-input {
   margin-bottom: 30px;
-  width: 300px;             /* 幅を広げる */
+  width: 435px;             /* 幅を広げる */
   font-size: 1.5em;         /* テキストとカレンダーアイコンを拡大 */
   padding: 12px 16px;       /* 内側余白を広げる */
   border: 2px solid #999;
   border-radius: 8px;
   box-sizing: border-box;
 }
+.label{font-size: large;
+
+}
 .date{
-   width: 300px;
+   width: 400px;
   height: 100px;
   font-size: 1.6em;
   padding: 10px 20px;
@@ -135,15 +143,21 @@ function applyEdits() {
 
 /* 表の見た目 */
 .time-table {
-  width: 300%;
+  width:150%;
   border-collapse: collapse;
   font-size: 1.2em;
 }
-.time-table th,
-.time-table td {
+.time-table th {
   border: 1px solid #888;
   padding: 16px;
   text-align: center;
+  font-size: 16px;
+}
+.time-table td{
+    border: 1px solid #888;
+  padding: 16px;
+  text-align: center;
+  font-size: 50px;
 }
 
 .apply-button {
